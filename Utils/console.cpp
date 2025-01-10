cacc0d10c7712cab5462cfc783f6069b6b65cf83
Muhamad Mamoun <muhamadmamoun@gmail.com>
2025-01-10 17:36:18 +0200

    chore: edit displayError function

--- Utils/console.cpp
+++ Utils/console.cpp
@@ -4,21 +4,15 @@
 
 void ConsUtils::displayError(const char* a_errorMessage)
 {
-    std::cout << "\r";
+    char timeBuffer[25] = {'\0'};
+    ConsUtils::getTime(timeBuffer, sizeof(timeBuffer));
+
     ConsUtils::changeConsoleColor(CONSOLE_COLOR_RED);
-    ConsUtils::displayTime();
-    std::cout << "[ERROR]: " << a_errorMessage << std::endl;
+    std::cout << "\r[" << timeBuffer << "]~[ERROR]: " << a_errorMessage << std::endl;
     ConsUtils::changeConsoleColor(CONSOLE_COLOR_WHITE);
 }
 
 void ConsUtils::changeConsoleColor(ConsUtils::consoleColor a_color)
 {
     std::cout << "\033[" << a_color << "m";
-}
-
-void ConsUtils::displayTime(void)
-{
-    char timeBuffer[25] = {'\0'};
-    ConsUtils::getTime(timeBuffer, sizeof(timeBuffer));
-    std::cout << timeBuffer;
 }
