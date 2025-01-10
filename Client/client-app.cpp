0a2e886bbc33e0d5a4a72cc8a87d236fceaf3e70
Muhamad Mamoun <muhamadmamoun@gmail.com>
2025-01-10 20:31:00 +0200

    style: enhance the code readability

--- Client/client-app.cpp
+++ Client/client-app.cpp
@@ -1,9 +1,34 @@
+/*
+=======================================================================================================================
+Author       : Mamoun
+Module       : Client-side Application
+File Name    : client-app.cpp
+Date Created : Jan 10, 2025
+Description  : Source file for the client-side application.
+=======================================================================================================================
+*/
+
+
+/*=====================================================================================================================
+                                               < Includes >
+=====================================================================================================================*/
+
 #include "../Utils/console.hpp"
 #include "../Utils/time.hpp"
 #include "client.hpp"
 #include <iostream>
 #include <cstring>
 #include <thread>
+
+/*=====================================================================================================================
+                                < Definitions and Static Configurations >
+=====================================================================================================================*/
+
+#define CLIENT_MESSAGE_BUFFER_SIZE 1024UL           /* The size of all the client message buffers. */
+
+/*=====================================================================================================================
+                                         < Functions Prototypes >
+=====================================================================================================================*/
 
 void parseArguments(int a_argCounter, char** a_argValues);
 void getServerIPv4Address(void);
@@ -14,29 +39,45 @@ void sendMessage(Client* a_myClient);
 void createMessageFrame(char* a_messageFrame, int a_frameSize, const char* a_messageContent);
 void receiveMessage(Client* a_myClient);
 
+/*=====================================================================================================================
+                                       < Shared Global Variables >
+=====================================================================================================================*/
+
 char* serverSocketAddress = nullptr;
 char* serverIPv4Address = nullptr;
 char* serverPortNumber = nullptr;
 char* clientName = nullptr;
 
+/*=====================================================================================================================
+                                          < Functions Definitions >
+=====================================================================================================================*/
+
+/*=====================================================================================================================
+ * [Function Name] : main
+ * [Description]   : The entry point for the server-side application.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns int.
+ ====================================================================================================================*/
 int main(int argc, char** argv)
 {
     Client* myClient = nullptr;
 
-    try {parseArguments(argc, argv);}
+    try {parseArguments(argc, argv);} /* If the user entered arguments, parse it. */
     catch(const char* a_exceptionMessage) {ConsUtils::displayError(a_exceptionMessage);};
 
+    /* If there any missed arguments, read it from the user. */
     if((serverSocketAddress == nullptr) && (serverIPv4Address == nullptr)) getServerIPv4Address();
     if((serverSocketAddress == nullptr) && (serverPortNumber == nullptr)) getServerPort();
     if(clientName == nullptr) getClientName();
 
     std::cout << "\nWelcome, " << clientName << "!" << std::endl;
-    std::cout << "Connected successfully." << std::endl << std::endl;
 
     try
     {
+        /* Create a client to connect to a specific IPv4 on a specific port. */
         if(serverSocketAddress) myClient = new Client(serverSocketAddress);
         else myClient = new Client(serverIPv4Address, serverPortNumber);
+        std::cout << "Connected successfully." << std::endl << std::endl;
         startSession(myClient);
     }
     catch(const char* a_exceptionMessage) {ConsUtils::displayError(a_exceptionMessage);}
@@ -52,6 +93,13 @@ int main(int argc, char** argv)
     return 0;
 }
 
+/*=====================================================================================================================
+ * [Function Name] : parseArguments
+ * [Description]   : Parse the user entered arguments.
+ * [Arguments]     : <a_argCounter>    -> The number of the entered arguments.
+ *                   <a_argValues>     -> Pointer to the entered arguments.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void parseArguments(int a_argCounter, char** a_argValues)
 {
     while(a_argCounter != 1) /* Ignore the 1st argument [Program Name]. */
@@ -87,6 +135,12 @@ void parseArguments(int a_argCounter, char** a_argValues)
     }
 }
 
+/*=====================================================================================================================
+ * [Function Name] : getServerIPv4Address
+ * [Description]   : Read the server IPv4 address from the user.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void getServerIPv4Address(void)
 {
     serverIPv4Address = new char[16]();
@@ -94,6 +148,12 @@ void getServerIPv4Address(void)
     std::cin >> serverIPv4Address;
 }
 
+/*=====================================================================================================================
+ * [Function Name] : floodToClients
+ * [Description]   : Read the server port number from the user.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void getServerPort(void)
 {
     serverPortNumber = new char[6]();
@@ -101,6 +161,12 @@ void getServerPort(void)
     std::cin >> serverPortNumber;
 }
 
+/*=====================================================================================================================
+ * [Function Name] : floodToClients
+ * [Description]   : Read the client name from the user.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void getClientName(void)
 {
     clientName = new char[50]();
@@ -108,8 +174,15 @@ void getClientName(void)
     std::cin >> clientName;
 }
 
+/*=====================================================================================================================
+ * [Function Name] : startSession
+ * [Description]   : Start the client-server session [sending/receiving].
+ * [Arguments]     : <a_myClient>             -> Pointer to the created client object.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void startSession(Client* a_myClient)
 {
+    /* Create two separated threads to handle the sending/receiving operations. */
     std::thread sendingThread(sendMessage, a_myClient);
     std::thread receivingThread(receiveMessage, a_myClient);
 
@@ -117,15 +190,23 @@ void startSession(Client* a_myClient)
     sendingThread.join();
 }
 
+/*=====================================================================================================================
+ * [Function Name] : sendMessage
+ * [Description]   : Send a message to the server.
+ * [Arguments]     : <a_myClient>             -> Pointer to the created client object.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void sendMessage(Client* a_myClient)
 {
-    char messageFrame[1024] = {'\0'};
-    char messageContent[1024] = {'\0'};
+    char messageFrame[CLIENT_MESSAGE_BUFFER_SIZE] = {'\0'};
+    char messageContent[CLIENT_MESSAGE_BUFFER_SIZE] = {'\0'};
 
     while(a_myClient->getConnectionStatus() == true)
     {
         std::cout << "\r>_ ";
-        std::cin.getline(messageContent, 1024);
+        std::cin.getline(messageContent, CLIENT_MESSAGE_BUFFER_SIZE);
+
+        /* If the user entered the client termination message, terminate the session. */
         if(!strcmp(messageContent, "goodbye")) a_myClient->setConnectionStatus(false);
         else if(strlen(messageContent)) 
         {
@@ -134,12 +215,21 @@ void sendMessage(Client* a_myClient)
         }
     }
 
+    /* Terminate the client session. */
     std::cout << "\nGoodbye, " << clientName << "." << std::endl;
-    createMessageFrame(messageFrame, 1024, "Left the group");
+    createMessageFrame(messageFrame, CLIENT_MESSAGE_BUFFER_SIZE, "Left the group");
     a_myClient->sendMessage(messageFrame, strlen(messageFrame));
     a_myClient->sendMessage("\0", sizeof("\0"));
 }
 
+/*=====================================================================================================================
+ * [Function Name] : createMessageFrame
+ * [Description]   : Prepare the message frame to be sent [current-time]~[client-name]: <message>.
+ * [Arguments]     : <a_messageFrame>    -> Pointer to the frame.
+ *                   <a_frameSize>       -> Frame size.
+ *                   <a_messageContent>  -> The buffer that contains the message.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void createMessageFrame(char* a_messageFrame, int a_frameSize, const char* a_messageContent)
 {
     char currentTime[25] = {'\0'};
@@ -147,13 +237,19 @@ void createMessageFrame(char* a_messageFrame, int a_frameSize, const char* a_mes
     snprintf(a_messageFrame, a_frameSize, "[%s]~[%s]: %s", currentTime, clientName, a_messageContent);
 }
 
+/*=====================================================================================================================
+ * [Function Name] : receiveMessage
+ * [Description]   : Receive message from the server.
+ * [Arguments]     : <a_myClient>             -> Pointer to the created client object.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void receiveMessage(Client* a_myClient)
 {
-    char messageFrame[1024] = {'\0'};
+    char messageFrame[CLIENT_MESSAGE_BUFFER_SIZE] = {'\0'};
     
     while(a_myClient->getConnectionStatus() == true)
     {
-        std::fill(messageFrame, (messageFrame + 1024), '\0');
+        std::fill(messageFrame, (messageFrame + CLIENT_MESSAGE_BUFFER_SIZE), '\0');
         a_myClient->receiveMessage(messageFrame, sizeof(messageFrame));
         if(strlen(messageFrame)) std::cout << "\r" << messageFrame << "\n\r>_ " << std::flush;
     }
--- Server/server-app.cpp
+++ Server/server-app.cpp
@@ -1,42 +1,83 @@
+/*
+=======================================================================================================================
+Author       : Mamoun
+Module       : Server-side Application
+File Name    : server-app.cpp
+Date Created : Jan 10, 2025
+Description  : Source file for the server-side application.
+=======================================================================================================================
+*/
+
+
+/*=====================================================================================================================
+                                               < Includes >
+=====================================================================================================================*/
+
 #include "../Utils/console.hpp"
 #include "server.hpp"
 #include <iostream>
 #include <cstring>
 #include <thread>
 
-#define SERVER_MAX_ALLOWED_CLIENT 10UL
+/*=====================================================================================================================
+                                < Definitions and Static Configurations >
+=====================================================================================================================*/
+
+#define SERVER_LISTENING_PORT 3232   /* The port that will be bound by the server to listen on it. */ 
+#define SERVER_MAX_ALLOWED_CLIENT 10UL     /* Maximum allowed clients to be handled by the server. */
+#define SERVER_MESSAGE_BUFFER_SIZE 1024UL           /* The size of all the server message buffers. */
+
+/*=====================================================================================================================
+                                         < Functions Prototypes >
+=====================================================================================================================*/
 
 void getUserInput(void);
 void listenToNewClient(Server* a_myServer);
 void receiveFromClient(Server* a_myServer, int a_clientIndex);
 void floodToClients(Server* a_myServer, int a_clientFileDescriptor, char* a_messageBuffer);
 
+/*=====================================================================================================================
+                                       < Shared Global Variables >
+=====================================================================================================================*/
+
 struct clientInfo
 {
     char name[20];
     bool activeClient;
     int fileDescriptor;
-    char messageBuffer[1024];
+    char messageBuffer[SERVER_MESSAGE_BUFFER_SIZE];
     std::thread* receiveMessageThread;
-}serverClients[SERVER_MAX_ALLOWED_CLIENT];
+}serverClients[SERVER_MAX_ALLOWED_CLIENT];                      /* Array hosts all server clients. */
 
-unsigned short currentClientsCounter = 0;
-bool activeSession = true;
+unsigned short currentClientsCounter = 0;             /* The number of the current active clients. */
+bool activeSession = true;                           /* Flag indicates the current session status. */
 
+/*=====================================================================================================================
+                                          < Functions Definitions >
+=====================================================================================================================*/
+
+/*=====================================================================================================================
+ * [Function Name] : main
+ * [Description]   : The entry point for the server-side application.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns int.
+ ====================================================================================================================*/
 int main(void)
 {
     Server* myServer = nullptr;
 
     try
     {
-        myServer = new Server(3232, SERVER_MAX_ALLOWED_CLIENT);
+        /* Create the server with a specific port and specific number of allowed clients. */
+        myServer = new Server(SERVER_LISTENING_PORT, SERVER_MAX_ALLOWED_CLIENT);
 
+        /* Create a thread to listen to new clients. */
         std::thread listenToNewClientThread(listenToNewClient, myServer);
-        std::cout << "\nThe server is created successfully and running." << std::endl;
+        std::cout << "\nThe server created successfully and is currently running." << std::endl;
         std::cout << "Press \"exit\" to terminate the session." << std::endl << std::endl;
-        std::cout << "Listenning..." << std::endl;
+        std::cout << "Listening..." << std::endl;
         listenToNewClientThread.detach();
-        getUserInput();
+        getUserInput();     /* Read the user input to terminate the session when he need. */
     }
     catch(const char* a_exceptionMessage) {ConsUtils::displayError(a_exceptionMessage);}
 
@@ -44,9 +85,15 @@ int main(void)
     return 0;
 }
 
+/*=====================================================================================================================
+ * [Function Name] : getUserInput
+ * [Description]   : Read the user input to terminate the session when he enter "exit".
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void getUserInput(void)
 {
-    char userInput[1024] = {'\0'};
+    char userInput[SERVER_MESSAGE_BUFFER_SIZE] = {'\0'};
 
     while(activeSession == true)
     {
@@ -55,6 +102,12 @@ void getUserInput(void)
     }
 }
 
+/*=====================================================================================================================
+ * [Function Name] : listenToNewClient
+ * [Description]   : Listen to clients for new connections.
+ * [Arguments]     : <a_myServer>      -> Pointer to the created server object.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void listenToNewClient(Server* a_myServer)
 {
     while(activeSession == true)
@@ -62,6 +115,8 @@ void listenToNewClient(Server* a_myServer)
         if(currentClientsCounter < SERVER_MAX_ALLOWED_CLIENT)
         {
             a_myServer->listenToNewClient(&serverClients[currentClientsCounter].fileDescriptor);
+
+            /* If a new client found, create a new separated thread to receive messages from him. */
             serverClients[currentClientsCounter].receiveMessageThread = new std::thread(receiveFromClient, a_myServer, currentClientsCounter);
             serverClients[currentClientsCounter].receiveMessageThread->detach();
             currentClientsCounter++;
@@ -69,17 +124,28 @@ void listenToNewClient(Server* a_myServer)
     }
 }
 
+/*=====================================================================================================================
+ * [Function Name] : receiveFromClient
+ * [Description]   : Receive messages from a specific client.
+ * [Arguments]     : <a_myServer>      -> Pointer to the created server object.
+ *                   <a_clientIndex>   -> The index of the client in the clients array.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void receiveFromClient(Server* a_myServer, int a_clientIndex)
 {
     serverClients[a_clientIndex].activeClient = true;
     
     while(serverClients[a_clientIndex].activeClient == true)
     {
-        std::fill(serverClients[a_clientIndex].messageBuffer, (serverClients[a_clientIndex].messageBuffer + 1024), '\0');
+        /* Clear the buffer for safe message handling. */
+        std::fill(serverClients[a_clientIndex].messageBuffer, (serverClients[a_clientIndex].messageBuffer + SERVER_MESSAGE_BUFFER_SIZE), '\0');
         a_myServer->receiveMessage(serverClients[a_clientIndex].fileDescriptor, serverClients[a_clientIndex].messageBuffer, sizeof(serverClients[a_clientIndex].messageBuffer));
+
+        /* If the termination message came from the user, change the status of this client. */
         if(!strcmp(serverClients[a_clientIndex].messageBuffer, "\0")) serverClients[a_clientIndex].activeClient = false;
         else
         {
+            /* Broadcast the message to all the other active clients. */
             floodToClients(a_myServer, serverClients[a_clientIndex].fileDescriptor, serverClients[a_clientIndex].messageBuffer);
             std::cout << serverClients[a_clientIndex].messageBuffer << std::endl;
         }
@@ -88,10 +154,19 @@ void receiveFromClient(Server* a_myServer, int a_clientIndex)
     delete serverClients[a_clientIndex].receiveMessageThread;
 }
 
+/*=====================================================================================================================
+ * [Function Name] : floodToClients
+ * [Description]   : Broadcast the message to all the other active clients.
+ * [Arguments]     : <a_myServer>             -> Pointer to the created server object.
+ *                   <a_clientFileDescriptor> -> The file descriptor for the sender client.
+ *                   <a_messageBuffer>        -> The buffer that contains the message.
+ * [return]        : The function returns void.
+ ====================================================================================================================*/
 void floodToClients(Server* a_myServer, int a_clientFileDescriptor, char* a_messageBuffer)
 {
     for(int clientIndex = 0; clientIndex <= currentClientsCounter; clientIndex++)
     {
+        /* If this client is active client and not the sender, send the message to him. */
         if((serverClients[clientIndex].fileDescriptor != a_clientFileDescriptor) && (serverClients[clientIndex].activeClient == true))
         {
             a_myServer->sendMessage(serverClients[clientIndex].fileDescriptor, a_messageBuffer, strlen(a_messageBuffer));
