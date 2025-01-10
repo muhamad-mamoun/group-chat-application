7ccbfbe0d9cc7ecc14289fdd79b9dba6d3d039e1
Muhamad Mamoun <muhamadmamoun@gmail.com>
2025-01-10 17:32:01 +0200

    chore: edit returned time format

--- Utils/time.cpp
+++ Utils/time.cpp
@@ -7,7 +7,7 @@ void ConsUtils::getTime(char* a_buffer, int a_bufferSize)
     std::time_t currentTime = std::chrono::high_resolution_clock::to_time_t(std::chrono::high_resolution_clock::now());
     tm* timeBroken = std::localtime(&currentTime);
 
-    snprintf(a_buffer, 1024, "[%d-%d-%d %d:%d:%d]", \
+    snprintf(a_buffer, 1024, "%d-%d-%d %d:%d:%d", \
     (timeBroken->tm_year + 1900), (timeBroken->tm_mon + 1), \
     timeBroken->tm_mday, timeBroken->tm_hour, timeBroken->tm_min, timeBroken->tm_sec);
 }
